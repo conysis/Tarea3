f53842112936bd7f0c2c03d1cb3e39e231813704
Alexander Goicoechea <alexander.goicoechea@uned.cr>
2021-08-02 00:41:32 -0600

    Agregar archivos de proyecto.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using CitasServer;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("GoicoSOFT gRPC 1.0");
+            Console.WriteLine("UNED - 2021");
+
+            var input = new HelloRequest { Name = "Alexander Goicoechea" };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine(reply.Message);
+        }
+    }
+}
